89314e837694a2c446fa87393647768805d431e8
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-15 00:50:20 +0200

    # Posr request for generate and save new short url using new validators and function
    Save the data in Mongo, validate data with the data in mongoDB

--- back-end/routers/urlsRouter.js
+++ back-end/routers/urlsRouter.js
@@ -1,31 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const path = require("path");
-const fs = require("fs");
-const UrlData = require('../DB/urlDataClass');
-const {updateUrlGetCount, isValidHttpUrl, generateId, checkNewUser} = require("./helpers");
+const path = require('path');
+const fs = require('fs');
+const { updateUrlGetCount } = require('./helpers');
+const { isValidHttpUrl, checkLongUrlByUserName } = require('../middlewares/validator');
+const { createAndSaveNewUrl } = require('../controller/urlData');
 
 // localhost:3000/api
 // const BASEURL = "http://localhost:3000/api";
 const BASEURL = "https://ramas-url-shortener.herokuapp.com/api";
 
 // Generates a new id and saves it in the database
-router.post("/", (req, res) => {
-    try {
-        const longUrl = req.body.longUrl;
-        const userName = req.body.userName;
-        checkNewUser(userName); //Open a new file if the user is new
-        if ( !isValidHttpUrl(longUrl)) throw {"status": 400, "messege": "invalid URL"};
-        const id = generateId(userName);
-        const shortUrl = `${BASEURL}/${userName}/${id}`;
-        const urlData = new UrlData(longUrl, id, shortUrl); //Create url data object
-        const saveResult = urlData.saveToUserDir(userName);
-        if(saveResult) res.send(saveResult); //The address has been shortened in the past.
-        else res.send(shortUrl); //New address return the new one.
-    } catch (error) {
-        throw {"status": error.status, "messege": error.messege};
-    }
-})
+router.post('/', isValidHttpUrl, checkLongUrlByUserName, createAndSaveNewUrl);
 
 //Receives a request from a short URL and redirects it to the original site (long URL), base on the DB
 router.get("/:userName/:id", (req, res) => {
