d15c05667285bf62521e94eb5f1d09bb0a57e618
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-14 22:02:00 +0200

    # check if url was shorten before as a middleware function

--- back-end/middlewares/validator.js
+++ back-end/middlewares/validator.js
@@ -1,3 +1,5 @@
+const UrlData = require('../models/urlData');
+
 //Checks if the url is valid
 exports.isValidHttpUrl = (req, res, next) => {
   let url;
@@ -11,4 +13,19 @@ exports.isValidHttpUrl = (req, res, next) => {
     throw { status: 400, messege: 'invalid URL' };
   }
   if (url.protocol === 'http:' || url.protocol === 'https:') next();
+};
+
+//Check if url was shorten before
+exports.checkLongUrlByUserName = async (req, res, next) => {
+  try {
+    const { longUrl, userName } = req.body;
+    const urlData = await UrlData.find({ userName }, { longUrl });
+    if (urlData !== null) {
+      //was shorten before, pass the short url next
+      req.shortUrl = urlData.shortUrl;
+    }
+    next();
+  } catch (error) {
+    throw { status: error.status, messege: error.messege };
+  }
 };
