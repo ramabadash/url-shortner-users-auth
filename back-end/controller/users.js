673ce25ca9d48819023e0755013fb495557f19d0
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-15 20:17:05 +0200

    #Create a login function using the login detail and create a token

--- back-end/controller/users.js
+++ back-end/controller/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 //Sign up
 exports.signUp = async (req, res, next) => {
@@ -6,6 +7,9 @@ exports.signUp = async (req, res, next) => {
     const { userName, password, email } = req.body;
     if (!userName || !email || !password) {
       throw { status: 400, messege: 'Must fiil all fields' };
+    }
+    if (await User.exists({ userName })) {
+      throw { status: 400, messege: 'User Name is taken' };
     }
     User.create({ userName, password, email })
       .then((newUser) => {
@@ -21,3 +25,31 @@ exports.signUp = async (req, res, next) => {
     next({ status: error.status, messege: error.messege });
   }
 };
+
+//Login
+exports.login = async (req, res, next) => {
+  try {
+    //Check user name details
+    const { userName, password, email } = req.body;
+    const userObj = await User.find({ userName, password, email });
+    if (userObj.length === 0) {
+      next({ status: 401, messege: 'Wrong details' });
+      return;
+    } else {
+      //Generate token
+      const user = { userName };
+      const token = generateAccessToken(user);
+      return res.status(200).cookie('token', token).send(true);
+    }
+  } catch (error) {
+    console.log(error);
+    next({ status: error.status, messege: error.messege });
+  }
+};
+
+//Generate token
+function generateAccessToken(user) {
+  return jwt.sign({ user }, 'secret', {
+    expiresIn: '3600s',
+  });
+}
--- back-end/routers/usersEntry.js
+++ back-end/routers/usersEntry.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signUp } = require('../controller/users');
-
+const { signUp, login } = require('../controller/users');
 // localhost:3000/entry
 
 //Sign up by newUser
 router.post('/signUp', signUp);
+//Login
+router.post('/login', login);
 
 module.exports = router;
