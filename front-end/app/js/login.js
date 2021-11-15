626504cb9f75b1ab664e7e71fa639516fb56e81a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-15 16:47:23 +0200

    # New form for signUp / login

---
+++ front-end/app/js/login.js
@@ -0,0 +1,7 @@
+// const loginBtn = document.getElementById('loginBtn');
+// const signUpButton = document.getElementById('signupBtn');
+
+// export function showPage() {
+//   document.querySelector('.entry').style.display = 'none';
+//   document.querySelector('#page').style.display = 'block';
+// }
