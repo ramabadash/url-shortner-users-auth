63571c6272fe51b867af70a7358ebe3ebbedcfd6
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-15 17:14:01 +0200

    Delete login.js

--- front-end/app/js/login.js
+++
@@ -1,7 +0,0 @@
-// const loginBtn = document.getElementById('loginBtn');
-// const signUpButton = document.getElementById('signupBtn');
-
-// export function showPage() {
-//   document.querySelector('.entry').style.display = 'none';
-//   document.querySelector('#page').style.display = 'block';
-// }
