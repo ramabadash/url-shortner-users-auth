7b63d81572641ef88080bd81d42d8bb5b1122d3a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-17 15:22:03 +0200

    # Front - add username, password & email validation

--- front-end/app/login.js
+++ front-end/app/login.js
@@ -16,7 +16,7 @@ const emailSignUp = document.getElementById('email-input-signup');
 const passwordSignUp = document.getElementById('password-input-signup');
 
 // Switch screens
-
+//Show signUp
 showSignUp.addEventListener('click', () => {
   signupForm.classList.toggle('hide');
   loginForm.classList.toggle('hide');
@@ -37,9 +37,18 @@ async function signUp() {
     const userName = userNameSignUp.value;
     const email = emailSignUp.value;
     const password = passwordSignUp.value;
+    // Validate inputs
+    const isValidDetails = validateUserDetails(userName, password, email);
+    if (isValidDetails !== true) {
+      clearSignUpInputs();
+      errorMessege(isValidDetails, signupForm);
+      return;
+    }
     const response = await axios.post(`${BASEURL}/entry/signUp/`, { userName, password, email });
+    clearSignUpInputs();
     showLogin();
   } catch (error) {
+    clearSignUpInputs();
     errorMessege(error.response.data.error, signupForm);
   }
 }
@@ -54,8 +63,10 @@ async function login() {
       userName,
       password,
     });
+    clearLoginInputs();
     window.location.replace(response.data);
   } catch (error) {
+    clearLoginInputs();
     errorMessege(error.response.data.error, loginForm);
   }
 }
@@ -68,4 +79,46 @@ function errorMessege(messege, element) {
   errorElem.classList.add('error-messege');
   element.appendChild(errorElem);
   setTimeout(() => errorElem.remove(), 5000);
+}
+/*---------- DOM RELATED ----------*/
+function clearSignUpInputs() {
+  userNameSignUp.value = '';
+  emailSignUp.value = '';
+  passwordSignUp.value = '';
+}
+function clearLoginInputs() {
+  userNameInputLogin.value = '';
+  passwordInputLogin.value = '';
+}
+
+/*---------- Validators ----------*/
+//Vlidate user details
+function validateUserDetails(username, password, email) {
+  if (!validateEmail(email)) {
+    return 'Not valid Email';
+  } else if (!validateUserName(username)) {
+    return 'Not valid userName';
+  } else if (!validatePassword(password)) {
+    return 'Not A strong password';
+  } else {
+    return true;
+  }
+}
+
+//Validate Email
+function validateEmail(emailAdress) {
+  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //Regex: valid email
+  return emailAdress.match(regexEmail);
+}
+
+//Validate userName
+function validateUserName(username) {
+  const usernameRegex = /^[a-zA-Z0-9]+$/; //Regex: At least 1 UperCase letter  + 1 lowerCase letter + Number
+  return usernameRegex.test(username);
+}
+
+//Validate paswword
+function validatePassword(password) {
+  let pattern = new RegExp('^(?=(.*[a-zA-Z]){1,})(?=(.*[0-9]){2,}).{8,}$'); //Regex: At least 8 characters with at least 2 numericals
+  return pattern.test(password);
 }
